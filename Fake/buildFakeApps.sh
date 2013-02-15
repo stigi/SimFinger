34af4809dd1cea76aa9a2f78f542500914024461
Ullrich Schäfer <ullrich@seidbereit.de>
2013-02-15 10:57:46 +0100

    Using iOS SDK 6.1 & waxsim to install the apps into the simulator

--- Fake/buildFakeApps.sh
+++ Fake/buildFakeApps.sh
@@ -2,14 +2,16 @@
 
 build() {
 	cd fake$1
-	xcodebuild -configuration Debug -sdk iphonesimulator4.2
+	xcodebuild install -configuration Debug -sdk iphonesimulator6.1
 	if [ "$2" ]
 	then
 	mkdir -p "../SimFinger/SimAppCollection/FakeApps/Fake$1/$2.app"
 	cp -rf "build/Debug-iphonesimulator/$2.app" "../SimFinger/SimAppCollection/FakeApps/Fake$1/"
+	waxsim "../SimFinger/SimAppCollection/FakeApps/Fake$1/$2.app" &
 	else
 	mkdir -p ../SimFinger/SimAppCollection/FakeApps/Fake$1/$1.app
 	cp -rf build/Debug-iphonesimulator/$1.app ../SimFinger/SimAppCollection/FakeApps/Fake$1/
+	waxsim "../SimFinger/SimAppCollection/FakeApps/Fake$1/$1.app" &
 	fi
 	cd ..
 }
